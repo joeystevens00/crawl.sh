97a3e1239c6f024ae424a5ad0bc126911661668e
jstevens00 <joeystevens00@gmail.com>
2017-02-01 21:53:12 -0500

    split argparse and exports off into their own files

---
+++ argparse.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+displayHelp() {
+	echo "Placeholder help"
+	sleep 1
+	less $0
+	exit 1
+}
+
+argParse() {
+	for i in "$@"; do
+	case $i in
+		-u=*|--url=*)
+			url="${i#*=}"
+			shift # past argument=value
+             ;;
+		-t=*|--threads=*)
+    		cthreads="${i#*=}"
+    		shift # past argument=value
+    		;;
+        -h|--help=*)
+    		help=true
+            shift # past argument=value
+        	;;
+    	*)
+	       	help=true # unknown option
+    		;;
+	esac
+	done
+
+	if [ "$help" == true ]; then displayHelp; fi
+
+	if [ -z "$url" ]; then
+		lastLinkChecked=$(head -1 $tmpfile)
+		url=$lastLinkChecked
+	fi
+	resp=$(curl -s $url)
+	crawlLinks "$resp" "$url" 
+}
---
+++ exports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export -f multithreaded
+export -f escapeUrl
+export -f getDateTime
+export -f executeQuery
+export -f log
+export -f getLinks
+export -f goDeeper
+export -f crawlLinks
+export -f ifNoDupesThenLog
+export -f checkForDupes
+export -f doNothing
+export -f removeWhiteSpace
+export -f cleanMysteryCharacters
+export -f overwriteTmpFile
