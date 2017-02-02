aaa7a1c2d28041353f9e145721fe70684ff055ad
jstevens00 <joeystevens00@gmail.com>
2017-02-01 21:24:10 -0500

    directory sanity fix

--- test/watch-sql-count.sh
+++ test/watch-sql-count.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Ensure directory sanity
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 source ../crawler.conf.sh
 
 watch "mysql --defaults-extra-file=$mysqlAuthFile --execute='SELECT COUNT(id) FROM links;'"
