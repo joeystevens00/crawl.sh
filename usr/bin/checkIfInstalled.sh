cdff24e395f8f88ca99917173cb590dbc9a43240
jstevens00 <joeystevens00@gmail.com>
2017-02-02 22:31:26 -0500

    aaa

--- usr/bin/checkIfInstalled.sh
+++ usr/bin/checkIfInstalled.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 checkIfInstalled() {
-	command -v $1 >/dev/null 2>&1 || { echo "I require $1 but it's not installed.  Aborting." >&2; exit 1; }
+	command -v $1 >/dev/null 2>&1 || { echo "false"; }
 }
 
 checkIfInstalled "$1"
